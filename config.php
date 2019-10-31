76a605d1831184c6ddb7d1c75a23c55e1411d2ec
DerBastler <50806700+DerBastler@users.noreply.github.com>
2019-10-31 08:47:37 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+//Database 	login credentials
+$hostname = "***";    
+$database =	"***";
+$db_login = "***";           
+$db_pass =  "***";  
+
+//get this Key from https://ipgeolocation.io/
+$apiKey = "xxxxxxxxxxxxxxxxxxxxxxxx";  
+?>
